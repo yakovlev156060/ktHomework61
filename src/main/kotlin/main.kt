e421ca9e2fcf37bd35dfa6606ad040bf568994a9
Yakovlev Alexandr <ventry2016@gmail.com>
2021-11-06 23:08:56 +0500

    November Commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -60,91 +60,87 @@ interface Attachment{
 
 public class AudioAttachment(
     override val type:String = "Audio",
-    val audio: Audio=Audio(),
-    val id: Int,
-    val ownerId: Int,
-    val artist:String,
-    val title:String,
-    val duration: Int,
-    val lyricsId:String,
-    val albumId: Int,
-    val genreId: Int,
-    val date: Int,
-    val noSearch: Boolean,
-    val isHq: Boolean
-
-):Attachment{
+    val audio: Audio=Audio()):Attachment{
 }
+/*
+class AudioAttachment(override val type: String="audio"
+val audio: Audio=Audio()) : Attachment
+*/
 
 public class PhotoAttachment(
     override val type:String = "Photo",
-    val photo: Photo=Photo(),
-    val id: Int,
-    val albumId: Int,
-    val userId: Int,
-    val text:String,
-    val date: Int,
-    val width: Int,
-    val height: Int
-):Attachment{
+    val photo: Photo=Photo()):Attachment{
 }
 
 public class PostedPhotoAttachment(
     override val type:String = "PostedPhoto",
-    val postedPhoto: PostedPhoto=PostedPhoto(),
-    val id: Int,
-    val ownerId: Int,
-    val photo_130:String,
-    val photo_604:String
-):Attachment{
+    val postedPhoto: PostedPhoto=PostedPhoto()):Attachment{
 }
 
 public class WikiPageAttachment(
     override val type:String = "WikiPage",
-    val wikiPage: WikiPage=WikiPage(),
-    val id: Int,
-    val groupId: Int,
-    val title:String
-):Attachment{
+    val wikiPage: WikiPage=WikiPage()):Attachment{
 }
 
 public class NoteAttachment(
     override val type:String = "Note",
-    val note: Note=Note(),
-    val id: Int,
-    val ownerId: Int,
-    val title:String,
-    val text:String,
-    val date: Int,
-    val comments: Int,
-    val readComments: Int,
-    val viewUrl:String,
-    val privacyView:String,
-    val privacyComment:String,
-    val canComment: Boolean,
-    val textWiki:String,
+    val note: Note=Note()
 ):Attachment{
 }
 
-public class Audio(){
+public class Audio(
+    val id: Int=0,
+    val ownerId: Int=0,
+    val artist:String="",
+    val title:String="",
+    val duration: Int=0,
+    val lyricsId:String="",
+    val albumId: Int=0,
+    val genreId: Int=0,
+    val date: Int=0,
+    val noSearch: Boolean=true,
+    val isHq: Boolean=true){
 
 }
 
-public class Photo(){
-
+public class Photo(
+    val id: Int=0,
+    val albumId: Int=0,
+    val userId: Int=0,
+    val text:String="",
+    val date: Int=0,
+    val width: Int=0,
+    val height: Int=0){
 }
 
-public class PostedPhoto(){
-
+public class PostedPhoto(
+    val id: Int=0,
+    val ownerId: Int=0,
+    val photo_130:String="",
+    val photo_604:String=""){
 }
 
-public class WikiPage(){
-
+public class WikiPage(
+    val id: Int=0,
+    val groupId: Int=0,
+    val title:String=""){
 }
 
-public class Note(){
-
+public class Note(
+    val id: Int=0,
+    val ownerId: Int=0,
+    val title:String="",
+    val text:String="",
+    val date: Int=0,
+    val comments: Int=0,
+    val readComments: Int=0,
+    val viewUrl:String="",
+    val privacyView:String="",
+    val privacyComment:String="",
+    val canComment: Boolean=true,
+    val textWiki:String=""){
 }
+
 public data class Comments(
     val count: Int = 0,
     val canPost: Boolean = true,
